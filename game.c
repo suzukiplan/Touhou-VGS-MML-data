7bde753ab181a5c6bb5a11c6cbff5bcfcbad208e
Yoji Suzuki <suzukiplan@users.noreply.github.com>
2015-03-01 11:51:04 +0900

    Revert "英語の時のタイトル表示位置を1ピクセル下にする"

--- game.c
+++ game.c
@@ -408,11 +408,11 @@ int vge_loop()
 			}
 			if(_list[i].dis) {
 				putfontSD(8,i*20+137+(int)base,"%3d.",(i+1));
-				putkanji(26,i*20+133+(_list==_listE?1:0)+(int)base,103,"%s",_list[i].text);
+				putkanji(26,i*20+133+(int)base,103,"%s",_list[i].text);
 			} else {
 				putfontS(8,i*20+137+(int)base,"%3d.",(i+1));
-				putkanji(27,i*20+134+(_list==_listE?1:0)+(int)base,1,"%s",_list[i].text);
-				putkanji(26,i*20+133+(_list==_listE?1:0)+(int)base,255,"%s",_list[i].text);
+				putkanji(27,i*20+134+(int)base,1,"%s",_list[i].text);
+				putkanji(26,i*20+133+(int)base,255,"%s",_list[i].text);
 			}
 		}
 	}
